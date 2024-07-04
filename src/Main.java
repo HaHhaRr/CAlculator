358fabbe87bb7b294cedd1308571eb13ade51f8c
zahar <patrakov.za@mail.ru>
2024-07-04 18:28:51 +0300

    Add following account

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,87 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        String answer = "Ответ: ";
         Scanner sc = new Scanner(System.in);
+        try {
+            System.out.println("Введите первое число:");
+            double a = sc.nextDouble();
 
-        System.out.println("Введите первое число:");
-        double a = sc.nextDouble();
-        sc.nextLine();
-        System.out.println("Введите знак операции:");
-        String operation = sc.nextLine();
-        switch (operation) {
-            case "+/-":
-                System.out.println(Operations.unaryMinus(a));
-                break;
-            case "%":
-                System.out.println(Operations.per(a));
-                break;
-            case "!":
-                System.out.println(Operations.fact(a));
-                break;
-        }
+            for (int i = 0; i < Integer.MAX_VALUE; i++) {
+                sc.nextLine();
+                System.out.println("Введите знак операции:");
+                String operation = sc.nextLine();
+                double op = 0;
+                switch (operation) {
+                    case "+/-":
+                        op = Operations.unaryMinus(a);
+                        System.out.println(answer);
+                        System.out.println(op);
+                        break;
+                    case "%":
+                        op = Operations.per(a);
+                        System.out.println(answer);
+                        System.out.println(op);
+                        break;
+                    case "!":
+                        op = Operations.fact(a);
+                        System.out.println(answer);
+                        System.out.println(op);
+                        break;
+                }
 
-        if (operation.equals("+") || operation.equals("-") || operation.equals("*") ||
-                operation.equals("/") || operation.equals("^") || operation.equals("!^")) {
-            System.out.println("Введите второе число:");
-            double b = sc.nextDouble();
-            switch (operation) {
-                case "+":
-                    System.out.println(Operations.sum(a, b));
-                    break;
-                case "-":
-                    System.out.println(Operations.sub(a, b));
-                    break;
-                case "*":
-                    System.out.println(Operations.mult(a, b));
-                    break;
-                case "/":
-                    Errors.checkDel(a, b);
-                    if (Errors.bool) {
-                        System.out.println(Operations.del(a, b));
+                if (operation.equals("+") || operation.equals("-") || operation.equals("*") ||
+                        operation.equals("/") || operation.equals("^") || operation.equals("!^")) {
+                    System.out.println("Введите второе число:");
+                    double b = sc.nextDouble();
+                    switch (operation) {
+                        case "+":
+                            op = Operations.sum(a, b);
+                            System.out.println(answer);
+                            System.out.println(op);
+                            break;
+                        case "-":
+                            op = Operations.sub(a, b);
+                            System.out.println(answer);
+                            System.out.println(op);
+                            break;
+                        case "*":
+                            op = Operations.mult(a, b);
+                            System.out.println(answer);
+                            System.out.println(op);
+                            break;
+                        case "/":
+                            Errors.checkDel(a, b);
+                            if (Errors.bool) {
+                                op = Operations.del(a, b);
+                                System.out.println(answer);
+                                System.out.println(op);
+                            }
+                            break;
+                        case "^":
+                            op = Operations.power(a, b);
+                            System.out.println(answer);
+                            System.out.println(op);
+                            break;
+                        case "!^":
+                            Errors.checkSqrt(a, b);
+                            if (Errors.bool) {
+                                op = Operations.sqrt(a, b);
+                                System.out.println(answer);
+                                System.out.println(op);
+                            }
+                            break;
                     }
+                }
+                a = op;
+                if (operation.equals(".")) {
                     break;
-                case "^":
-                    System.out.println(Operations.power(a, b));
-                    break;
-                case "!^":
-                    Errors.checkSqrt(a, b);
-                    if (Errors.bool) {
-                        System.out.println(Operations.sqrt(a, b));
-                    }
-                    break;
+                }
             }
+        } catch (InputMismatchException ime) {
+            System.out.println("Найден недопустимый символ");
         }
     }
 }
--- src/Operations.java
+++ src/Operations.java
@@ -89,5 +89,4 @@ public class Operations {
             return res;
         }
     }
-
 }
